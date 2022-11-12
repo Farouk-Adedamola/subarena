e2c5109b3ba3f7b25f177327b331e8eb2658bc62
Farouk adedamola <faroukadedamola1@gmail.com>
2022-11-12 01:25:54 +0100

    new changes commit

--- src/Components/Wedo/Wedo.js
+++ src/Components/Wedo/Wedo.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import { list } from "./data";
+import classes from "./wedo.module.css";
 const Wedo = () => {
   return (
     <Fragment>
@@ -7,8 +8,10 @@ const Wedo = () => {
         {list.map((service) => {
           const { id, tag, name, text, css } = service;
           return (
-            <div style={css} key={id}>
-              <p>{tag}</p>
+            <div style={css} key={id} className={classes.container}>
+              <h1>
+                <span>{tag}</span>
+              </h1>
               <p>{name}</p>
               <p>{text}</p>
             </div>
