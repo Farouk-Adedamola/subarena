f8ab25b742a362a824fa1c71fe8120cdee4289bc
Farouk adedamola <faroukadedamola1@gmail.com>
2022-11-12 09:24:39 +0100

    commit new changes

--- src/Components/Wedo/Wedo.js
+++ src/Components/Wedo/Wedo.js
@@ -5,13 +5,21 @@ const Wedo = () => {
   return (
     <Fragment>
       <div>
+        <div className={classes.wrapper}>
+          <div className={classes.services}>
+            <h1>services</h1>
+            <div className={classes.line}></div>
+          </div>
+          <div className={classes.what}>our services</div>
+        </div>
         {list.map((service) => {
-          const { id, tag, name, text, css } = service;
+          const { id, tag, name, text, css, work } = service;
           return (
             <div style={css} key={id} className={classes.container}>
               <h1>
                 <span>{tag}</span>
               </h1>
+              <h4>{work}</h4>
               <p>{name}</p>
               <p>{text}</p>
             </div>
