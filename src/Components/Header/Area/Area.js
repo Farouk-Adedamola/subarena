76d66fa92a106f15862490d155b386e1277ffd4b
Farouk adedamola <faroukadedamola1@gmail.com>
2022-11-17 00:24:33 +0100

    commit changes

--- src/Components/Header/Area/Area.js
+++ src/Components/Header/Area/Area.js
@@ -1,16 +1,47 @@
 import React, { Fragment } from "react";
 import classes from "./Area.module.css";
+import TypewriterComponent from "typewriter-effect";
 const Area = () => {
   return (
     <Fragment>
       <div className={classes.area}>
         <div className={classes.main}>
-          <h2>Managed IT Services You Can Trust</h2>
+          <TypewriterComponent
+            className={classes.typewriter}
+            // react typewriter using options
+            options={{
+              strings: [
+                "Managed IT Services You Can Trust",
+                "From idea to product",
+                "End to End Development",
+              ],
+              delay: 100,
+              deleteSpeed: 150,
+              autoStart: true,
+              loop: true,
+            }}
+            // react typewriter using methods
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Managed IT Services You Can Trust")
+                .pauseFor(2000)
+                .deleteAll()
+                .pauseFor(1000)
+                .typeString("From idea to product")
+                .pauseFor(2000)
+                .deleteAll()
+                .pauseFor(2000)
+                .typeString("End to End Development")
+
+                .start();
+            }}
+          />
+          {/* <h2>Managed IT Services You Can Trust</h2>
           <p>
             subarena is an affiliated site of <span>manage connect</span> you
             can trust for delivering VTU website packages all in different
             packages
-          </p>
+          </p> */}
         </div>
         <button className={classes.button}>
           <a href="">Learn more</a>
